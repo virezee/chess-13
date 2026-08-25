6be4fc4583542c774b821e87c8fa494bcb1b02f1
Zee <56460839+virezee@users.noreply.github.com>
2026-08-25 14:42:50 +0700

    hotfix

--- src/features/game/components/Board.tsx
+++ src/features/game/components/Board.tsx
@@ -5,7 +5,7 @@ import { AUTO } from '@/constants/display'
 import { clickSquares } from '@/features/game/engine/turn'
 import { Grid } from './board/Grid'
 import { mark } from '../lib/annotation'
-import { emperorTimeline, marshalTimeline } from '../lib/trace'
+import { emperorFlag, riposteFlag } from '../lib/trace'
 import { useFlip } from '@/lib/flip'
 
 type BoardProps = {
@@ -52,7 +52,7 @@ export function Board(props: BoardProps) {
         ]}
         marks={marks}
         arrows={arrows}
-        trace={[...emperorTimeline(position), ...marshalTimeline(position)]}
+        trace={[...emperorFlag(position), ...riposteFlag(position)]}
         isFlipped={isFlipped}
         result={result}
         onSelect={select}
