fa06bcc9961db53656a2185359d63ea5da77411c
Zee <56460839+virezee@users.noreply.github.com>
2026-09-04 18:06:47 +0700

    static

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,6 @@
 import type { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: 'https://chess-13-epoch.vercel.app', changeFrequency: 'monthly', priority: 1 },
